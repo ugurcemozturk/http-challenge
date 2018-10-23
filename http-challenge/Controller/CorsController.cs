86367817951fe44a43ce46a98ca6ba2969e23002
Unknown <ugurcemozturk@gmail.com>
2018-10-23 19:57:22 +0300

    Cors and Auth controller added
    Only auth controller has content

---
+++ http-challenge/Controller/CorsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace http_challenge
+{
+    public class CorsController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return Ok();
+
+        }
+    }
+}
