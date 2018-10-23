42f6916e2fc5dfc2bc88848a70180a44d57ce8ac
Unknown <ugurcemozturk@gmail.com>
2018-10-23 19:58:49 +0300

    Response Builder added, a simple one

---
+++ http-challenge/Builders/ApiResponseBuilder.cs
@@ -0,0 +1,30 @@
+﻿using http_challenge.Core.Domain;
+
+namespace http_challenge.Builders
+{
+    public static class ApiResponseBuilder
+    {
+        public static ApiResponse CreateBuilder()
+        {
+            return new ApiResponse();
+        }
+
+        public static ApiResponse WithMessage(this ApiResponse response, string message)
+        {
+            response.Message = message;
+            return response;
+        }
+
+        public static ApiResponse ToEndpoint(this ApiResponse response, string endpoint)
+        {
+            response.Next = endpoint;
+            return response;
+        }
+
+        public static ApiResponse WithMethod(this ApiResponse response, string method)
+        {
+            response.Method = method;
+            return response;
+        }
+    }
+}
