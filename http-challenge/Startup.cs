d7728bd103ef732550012411b7e4bef76680cd71
Unknown <ugurcemozturk@gmail.com>
2018-10-26 17:08:25 +0300

    ICorsService injection

--- http-challenge/Startup.cs
+++ http-challenge/Startup.cs
@@ -1,8 +1,12 @@
-﻿
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using CorsService = http_challenge.Service.CorsService;
 
 namespace http_challenge
 {
@@ -18,7 +22,11 @@ namespace http_challenge
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();}
+            services.AddSingleton<ICorsService, CorsService>();
+
+            services.AddMvc();
+            services.AddCors();
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
