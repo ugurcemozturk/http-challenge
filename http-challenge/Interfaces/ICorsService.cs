be08832a44c75239d4afbd17875f8024536a8a94
Unknown <ugurcemozturk@gmail.com>
2018-10-26 17:09:45 +0300

    ADDED Cors service

---
+++ http-challenge/Interfaces/ICorsService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+
+namespace http_challenge
+{
+    public interface ICorsService
+    {
+        IEnumerable<string> GetCorsOrigins();
+        IEnumerable<string> GetCorsMethods();
+
+        bool CheckRequestOriginHeader(HttpRequest request);
+        bool CheckRequestHeadersHeader(HttpRequest request);
+        bool CheckRequestMethodsHeader(HttpRequest request);
+    }
+}
--- http-challenge/Service/CorsService.cs
+++ http-challenge/Service/CorsService.cs
@@ -1,7 +1,50 @@
-﻿namespace http_challenge.Service
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace http_challenge.Service
 {
-    public class CorsService
+    public class CorsService : ICorsService
     {
-        
+        private StringValues _corsHeader;
+
+        //TODO: Consider to use Code Contracts for null check
+        //TODO: Add this shit as a resource.
+        public IEnumerable<string> GetCorsOrigins() => new List<string>
+        {
+            "http://demo.com",
+            "https://demo.com"
+        };
+
+        public IEnumerable<string> GetCorsMethods() => new List<string>
+        {
+            HttpMethods.Get,
+            HttpMethods.Post,
+            HttpMethods.Options
+        };
+
+        public bool CheckRequestOriginHeader(HttpRequest request)
+        {
+            request.Headers.TryGetValue(CorsConstants.Origin, out _corsHeader);
+
+            return _corsHeader.Any() && GetCorsOrigins().Contains(_corsHeader[0]);
+        }
+
+        public bool CheckRequestMethodsHeader(HttpRequest request)
+        {
+            request.Headers.TryGetValue(CorsConstants.AccessControlRequestMethod, out _corsHeader);
+
+            return _corsHeader.Any() && GetCorsMethods().Contains(_corsHeader[0]);
+        }
+
+
+        public bool CheckRequestHeadersHeader(HttpRequest request)
+        {
+            request.Headers.TryGetValue(CorsConstants.Origin, out _corsHeader);
+
+            return _corsHeader.Any() && GetCorsOrigins().Contains(_corsHeader[0]);
+        }
     }
 }
